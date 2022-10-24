bd43273ac113842275631a82af46065fc9ea1e92
vledesma <v.ledesmaborrego@gmail.com>
2022-10-24 18:32:15 +0200

    app js in modulo-3

--- playground/js/app.js
+++ playground/js/app.js
@@ -24,17 +24,43 @@
 
 //Otra manera de plantearlo
 
-var hotel = {
-    name: "",
-    location: "",
-    description: "",
+// var hotel = {
+//     name: "",
+//     location: "",
+//     description: "",
+// };
+
+// hotel["name"] = prompt("Nombre del hotel");
+// document.getElementById("hotel-name").innerHTML = "Hotel " + hotel["name"];
+
+// hotel["location"] = prompt("localización del hotel");
+// document.getElementById("hotel-location").innerHTML = hotel["location"];
+
+// hotel["description"] = prompt("Descripción del hotel");
+// document.getElementById("hotel-description").innerHTML = hotel["description"];
+
+// //operadores
+// const user1 = {
+//     name: "john"
+// };
+
+// const user2={
+//     name: "john"
+// };
+// console.log(user1 === user2);//false
+// console.log(user1.name === user2.name);//true, tiene en cuenta el contenido
+
+// const a = 5;
+// const esMayorQue5 = a > 5; // esMayorQue5 tendrá valor false
+// const esMayorOIgualA5 = a >= 5; // esMayorOIgualA5 tendrá valor true
+// const esMenorQue5 = (a - 1) < 5; // esMenorQue5 tendrá valor true;
+// const esMenorOIgualA5 = a <= 5; // esMenorOIgualA5 tendrá valor true;
+
+const booking = {
+    count: 5,
+    price: 127.95
 };
 
-hotel["name"] = prompt("Nombre del hotel");
-document.getElementById("hotel-name").innerHTML = "Hotel " + hotel["name"];
+let total = booking.count * booking.price;
 
-hotel["location"] = prompt("localización del hotel");
-document.getElementById("hotel-location").innerHTML = hotel["location"];
 
-hotel["description"] = prompt("Descripción del hotel");
-document.getElementById("hotel-description").innerHTML = hotel["description"];
