8de3cc36032e2d03f533a3de93181b5e22765783
vledesma <v.ledesmaborrego@gmail.com>
2023-03-12 19:08:49 +0100

    parcel again

---
+++ modulo-8/src/client-business.js
@@ -0,0 +1,31 @@
+import { getAccounts } from "./data-business";
+import { getClientAccountsElement } from "./account-business";
+
+function getClientElement(client) {
+    const allAccounts = getAccounts();
+    const clientAccounts = [];
+    for (let account of allAccounts) {
+        if (account.clientId === client.id) {
+            clientAccounts.push(account);
+        }
+    }
+    const node = getClientNode(client);
+    const ul = getClientAccountsElement(clientAccounts);
+
+    node.appendChild(ul);
+
+    return node;
+}
+
+function getClientNode(client) {
+    const li = document.createElement("li");
+    li.append(getFullName(client));
+
+    return(li);
+}
+
+function getFullName(client) {
+    return client.first_name + " " + client.last_name;
+}
+
+export { getClientElement };
---
+++ modulo-8/src/index.js
@@ -0,0 +1,5 @@
+import { printClientsAccounts } from "./client-list-business";
+
+window.onload = function() {
+    printClientsAccounts();
+}
